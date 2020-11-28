ee63ca866cb71a293b607e25b905dc8726e2c518
Azmah-Bad <56384031+azmah-bad@users.noreply.github.com>
2020-11-28 22:59:11 +0100

    🛣 fixed a testing bug and added documentation

--- tests/router_test.py
+++ tests/router_test.py
@@ -2,6 +2,7 @@ import unittest
 from packages import Router
 import numpy as np
 import math
+import time
 
 
 class RouterTest(unittest.TestCase):
@@ -11,7 +12,7 @@ class RouterTest(unittest.TestCase):
                           [2, math.inf, math.inf, 1],
                           [math.inf, 3, 1, math.inf]])
 
-    AdjMatrixBi = np.array([[math.inf, 1, 2,        5],
+    AdjMatrixBi = np.array([[math.inf, 1, 2, 5],
                             [5, math.inf, math.inf, 3],
                             [1, math.inf, math.inf, 1],
                             [math.inf, 3, 1, math.inf]])
@@ -23,17 +24,37 @@ class RouterTest(unittest.TestCase):
 
     def test_dijkstra(self):
         mRouter = Router(self.RouterID, self.AdjMatrix, self.DemandMatrix)
+        start = time.time()
         mRouter.dijkstra()
+        end = time.time()
+        print(f"calculated Dijkstra in {end - start}")
         self.assertEqual(mRouter.shortest_paths[1], ([1], 1))
         self.assertEqual(mRouter.shortest_paths[2], ([2], 2))
         self.assertEqual(mRouter.shortest_paths[3], ([2, 3], 3))
 
     def test_dijkstra_unequal_bidirectional_weight(self):
         mRouter = Router(self.RouterID, self.AdjMatrixBi, self.DemandMatrix)
+        start = time.time()
         mRouter.dijkstra()
+        end = time.time()
+        print(f"calculated Dijkstra in {end-start}")
         self.assertEqual(mRouter.shortest_paths[1], ([1], 1))
         self.assertEqual(mRouter.shortest_paths[2], ([2], 2))
         self.assertEqual(mRouter.shortest_paths[3], ([2, 3], 3))
+
+    def test_dijkstra_from_b(self):
+        mRouter = Router(1, self.AdjMatrix, self.DemandMatrix)
+        start = time.time()
+        mRouter.dijkstra()
+        end = time.time()
+        print(f"calculated Dijkstra in {end - start}")
+        self.assertEqual(mRouter.shortest_paths[0], ([0], 1))
+        self.assertEqual(mRouter.shortest_paths[2], ([0, 2], 3))
+        self.assertEqual(mRouter.shortest_paths[3], ([3], 3))
+
+    def test_getNeighbors(self):
+        mRouter = Router(self.RouterID, self.AdjMatrix, self.DemandMatrix)
+        self.assertListEqual(mRouter.neighbors, [1, 2])
 
 
 if __name__ == '__main__':
