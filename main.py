bcc5fbcfbbca0383a445513fc0b56e3bdca1d1f1
Bilel S <bilel.saghrouchni@gmail.com>
2021-01-03 12:32:49 +0100

    add a debug function

--- main.py
+++ main.py
@@ -3,14 +3,32 @@
 from packages.router import Router
 from packages.network import Network
 from packages.functions import *
+from termcolor import colored
 import numpy as np
 import math
+
+def printDifference(mat1,mat2):
+    """
+    print a matrix in a different way where a value has been changed, help for debug
+    """
+    print("[")
+    for i in range (len(mat1)):
+        add = []
+        for j in range (len(mat1)):
+            if mat1[i][j] != mat2[i][j]:
+                add.append( "+" + str(mat2[i][j]) ) if mat1[i][j] < mat2[i][j] else add.append( "_" + str(mat2[i][j]) )
+            else:
+                add.append(str(mat2[i][j]))
+        print(add)
+    print("]")
 
 def main(network, max_iter):
     iter = 0
     network.nDijkstra()
-    print("\n\n----- Adjacency Matrix before -----\n", network.AdjacencyMatrix)
-    print("\n\n----- Load Matrix before -----\n", network.LoadMatrix)
+    load_before = np.around(network.LoadMatrix, decimals=1)
+    adj_before = np.around(network.AdjacencyMatrix, decimals=1)
+    print("\n\n----- Adjacency Matrix before -----\n", adj_before)
+    print("\n\n----- Load Matrix before -----\n", load_before)
 
     if isSaturated(network.LoadMatrix) == -1:
         disturbNetwork(network)
@@ -31,15 +49,15 @@ def main(network, max_iter):
     color_green = '\033[32m'
     print(color_red + "\n\n----- Adjacency Matrix after -----") if isSaturated(network.LoadMatrix) != -1 else print(color_green + "\n\n----- Adjacency Matrix after -----")
     print('\033[0m')
-    print(network.AdjacencyMatrix)
+    printDifference(adj_before,network.AdjacencyMatrix)
     print(color_red + "\n\n----- Load Matrix after -----") if isSaturated(network.LoadMatrix) != -1 else print(color_green + "\n\n----- Load Matrix after -----")
     print('\033[0m')
-    print(network.LoadMatrix)
+    printDifference(load_before,network.LoadMatrix)
 
 
 if __name__ == "__main__":
     mat_adj = computeModel("inputs/france.gml")
-    MAX_ITER = 50
+    MAX_ITER = 200
 
     """Test with a simple network"""
     mat_adj2_cor = np.array([
